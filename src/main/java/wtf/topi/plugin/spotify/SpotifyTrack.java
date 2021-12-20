6f8c746935a82772ef8c0b5a9f71daf1094c9e0a
TopiSenpai <tobi@strasserhof.com>
2021-12-20 01:58:04 +0100

    bump version, use correct identifier & fixed uri

--- src/main/java/wtf/topi/plugin/spotify/SpotifyTrack.java
+++ src/main/java/wtf/topi/plugin/spotify/SpotifyTrack.java
@@ -10,41 +10,41 @@ import se.michaelthelin.spotify.model_objects.specification.ArtistSimplified;
 import se.michaelthelin.spotify.model_objects.specification.Track;
 import se.michaelthelin.spotify.model_objects.specification.TrackSimplified;
 
-public class SpotifyTrack extends DelegatedAudioTrack{
+public class SpotifyTrack extends DelegatedAudioTrack {
 
-	private final AudioPlayerManager manager;
-	private final AudioSourceManager sourceManager;
+    private final AudioPlayerManager manager;
+    private final AudioSourceManager sourceManager;
 
-	public SpotifyTrack(String name, ArtistSimplified[] artists, Integer trackDuration, String trackURI, AudioPlayerManager manager, @NonNull AudioSourceManager sourceManager){
-		this(new AudioTrackInfo(name, artists[0].getName(), trackDuration.longValue(), "ytsearch:" + name + " " + artists[0].getName(), false, trackURI), manager, sourceManager);
-	}
+    public SpotifyTrack(String title, String identifier, ArtistSimplified[] artists, Integer trackDuration, AudioPlayerManager manager, @NonNull AudioSourceManager sourceManager) {
+        this(new AudioTrackInfo(title, artists[0].getName(), trackDuration.longValue(), identifier, false, "https://open.spotify.com/track/" + identifier), manager, sourceManager);
+    }
 
-	public SpotifyTrack(AudioTrackInfo trackInfo, AudioPlayerManager manager, AudioSourceManager sourceManager){
-		super(trackInfo);
-		this.manager = manager;
-		this.sourceManager = sourceManager;
-	}
+    public SpotifyTrack(AudioTrackInfo trackInfo, AudioPlayerManager manager, AudioSourceManager sourceManager) {
+        super(trackInfo);
+        this.manager = manager;
+        this.sourceManager = sourceManager;
+    }
 
-	public static SpotifyTrack of(TrackSimplified track, AudioPlayerManager manager, AudioSourceManager sourceManager){
-		return new SpotifyTrack(track.getName(), track.getArtists(), track.getDurationMs(), track.getUri(), manager, sourceManager);
-	}
+    public static SpotifyTrack of(TrackSimplified track, AudioPlayerManager manager, AudioSourceManager sourceManager) {
+        return new SpotifyTrack(track.getName(), track.getId(), track.getArtists(), track.getDurationMs(), manager, sourceManager);
+    }
 
-	public static SpotifyTrack of(Track track, AudioPlayerManager manager, AudioSourceManager sourceManager){
-		return new SpotifyTrack(track.getName(), track.getArtists(), track.getDurationMs(), track.getUri(), manager, sourceManager);
-	}
+    public static SpotifyTrack of(Track track, AudioPlayerManager manager, AudioSourceManager sourceManager) {
+        return new SpotifyTrack(track.getName(), track.getId(), track.getArtists(), track.getDurationMs(), manager, sourceManager);
+    }
 
-	@Override
-	public void process(LocalAudioTrackExecutor executor) throws Exception{
-		var track = this.manager.source(YoutubeAudioSourceManager.class).loadItem(this.manager, new AudioReference(this.trackInfo.identifier, null));
-		if (track instanceof AudioPlaylist){
-			track = ((AudioPlaylist) track).getTracks().get(0);
-		}
-		processDelegate((InternalAudioTrack) track, executor);
-	}
+    @Override
+    public void process(LocalAudioTrackExecutor executor) throws Exception {
+        var track = this.manager.source(YoutubeAudioSourceManager.class).loadItem(this.manager, new AudioReference("ytsearch:" + trackInfo.title + " " + trackInfo.author, null));
+        if (track instanceof AudioPlaylist) {
+            track = ((AudioPlaylist) track).getTracks().get(0);
+        }
+        processDelegate((InternalAudioTrack) track, executor);
+    }
 
-	@Override
-	public AudioSourceManager getSourceManager(){
-		return this.sourceManager;
-	}
+    @Override
+    public AudioSourceManager getSourceManager() {
+        return this.sourceManager;
+    }
 
 }
